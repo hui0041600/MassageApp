239d641419f3aac4c63de402d90ca8c4823a9152
hui0041600 <146614326+hui0041600@users.noreply.github.com>
2025-05-02 16:55:01 -0400

    Update AdminScreen.kt

--- app/src/main/java/com/example/massageapp/AdminScreen.kt
+++ app/src/main/java/com/example/massageapp/AdminScreen.kt
@@ -3,6 +3,8 @@ package com.example.massageapp
 import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -19,8 +21,8 @@ fun AdminScreen(navController: NavController) {
     val coroutineScope = rememberCoroutineScope()
 
     var bookings by remember { mutableStateOf(listOf<Booking>()) }
+    val scrollState = rememberScrollState()
 
-    // ALL APPOINTMENT INFO
     LaunchedEffect(Unit) {
         FirestoreService.listenToAllBookings { updatedBookings ->
             bookings = updatedBookings.sortedBy { it.date + it.time }
@@ -34,6 +36,7 @@ fun AdminScreen(navController: NavController) {
         Column(
             modifier = Modifier
                 .padding(innerPadding)
+                .verticalScroll(scrollState)
                 .padding(16.dp)
         ) {
             Text("Admin - All Bookings", fontSize = 24.sp)
